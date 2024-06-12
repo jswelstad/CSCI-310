73e736de60e9f4775d3bf58405e1836d5c542c52
Jack <jkswel@gmail.com>
2024-06-12 11:42:11 -0600

    HW12

---
+++ Homeworks/HW12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HW12Exceptions
+{
+
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+            
+        }
+    }
+    
+    public class HW12Exceptions
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Trying");
+                Console.WriteLine("Please enter the word Rick");
+
+                string word = Console.ReadLine();
+                
+                CustomException e = new CustomException("Haha I trolled you... Exception thrown");
+                e.HelpLink = "https://cl.gy/XWop";
+
+                if (word == "Rick")
+                {
+                    throw e;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception caught: {e}");
+                Console.WriteLine($"Follow this link for help: {e.HelpLink}");
+            }
+            
+        }
+    }
+}
